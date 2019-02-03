29ce91b8fa37c623757a72c514478d70a7fbdc05
YTC <qazxcv367tasi@gmail.com>
2019-02-03 14:12:29 +0800

    add get salary

--- js/content/catchEl.js
+++ js/content/catchEl.js
@@ -6,6 +6,7 @@ function getCname() {
 
 function getlawcount() {
     company = getCname()
+    company = company.replace(/\//g,"")
     reqStr = `http://localhost:8080/law/${company}`
     return new Promise((resolve,reject)=> {
         lowcount = $.getJSON(reqStr)
@@ -24,7 +25,34 @@ function getWelfare() {
 }
 
 function getSalary() {
+    s = document.getElementsByClassName('salary')[0].innerText
+    rangeMonth = /\d+\,\d+\~\d+\,\d+/
+    rangeHour = /\d+\~\d+/
+    staticMonth = /\d+\,\d+/
+    staticHourOrUp4 = /\d+/
+    
+    rm = s.match(rangeMonth)
+    rh = s.match(rangeHour)
+    sm = s.match(staticMonth)
+    sh4 = s.match(staticHourOrUp4)
+    
+    if (rm != null){
+        salary =rm[0]
+    }
+    if (rm == null && rh != null){
+        salary =rh[0]
+    }
+    if (sm != null && rm ==null){
+        salary =sm[0]
+    }
+    if (sm ==null && rm ==null && rh==null && sh4 != null){
+        salary =sh4[0]
+    }
+    
+    salary = salary.replace(/\,/g,"")
+    reqStr = `http://localhost:8080/welfare/`
 
+    return salary
 }
 
 function getJobcategory() {
@@ -36,12 +64,13 @@ function caculate(text) {
 }
 
 company = getCname()
+salary = getSalary()
 
 Promise.all([getlawcount(), getWelfare()]).then(
     function(para){
         records = para[0].records.length
         welfare = para[1].message
-        card = new helperCard(company, records , welfare, "9", "8")
+        card = new helperCard(company, records , welfare, salary, "8")
         card.init()
         card.listener() 
         console.log("ok")
